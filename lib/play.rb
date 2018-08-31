4ef3a3d0fc6651c80be65152bda00fa9674eb168
Mary Lark <mary@marylark.com>
2018-08-31 19:04:04 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -49,7 +49,6 @@ def turn (board)
   index = input_to_index(input) #just subtracts 1 from any value
   if (valid_move?(board, index))
     move(board, index, token)
-    iter += 1
     #token = get_new_token(token)
   end
 end
